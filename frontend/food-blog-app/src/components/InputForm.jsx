ec827c9e597379ad00fc36c04eb20f3919416064
SAURAV KUMAR <saurav8757462991@gmail.com>
2026-02-17 21:54:35 +0530

    Update InputForm.jsx

--- frontend/food-blog-app/src/components/InputForm.jsx
+++ frontend/food-blog-app/src/components/InputForm.jsx
@@ -13,7 +13,7 @@ export default function InputForm({ setIsOpen }) {
     let endpoint = isSignUp ? "signUp" : "login";
 
     try {
-      const res = await axios.post(`http://localhost:5000/${endpoint}`, {
+      const res = await axios.post(`https://foodrecipe-backend-layw.onrender.com/${endpoint}`, {
         email,
         password,
       });
